b65f72957bf74f757584744da4bf3817d4df3f1e
Tyson Sorensen <tysonsorensen3@gmail.com>
2015-09-17 11:12:32 -0600

    added controller functions and factory; html and css design

--- client/js/app.js
+++ client/js/app.js
@@ -2,15 +2,81 @@ angular.module('RepsApp', [
   'RepsAppControllers'
 ]);
 
-angular.module('RepsAppControllers', [])
-  .controller('MainCtrl', function($http) {
+angular.module('RepsAppControllers', [
+  'repsService'
+])
+  .controller('MainCtrl', function(reps) {
     this.reps = [];
 
     this.searchByZip = function(zip) {
-      $http
-        .get('http://dgm-representatives.herokuapp.com/all/by-zip/' + zip)
-        .then(function(res) {
-          this.reps = res.data;
-        }.bind(this));
+      reps.allByZip(zip).then(function(data) {
+        this.reps = data;
+      }.bind(this));
+    };
+
+    this.searchRepsByName = function(name) {
+      reps.repsByName(name).then(function(data) {
+        this.reps = data;
+      }.bind(this));
+    };
+
+    this.searchRepsByState = function(state) {
+      reps.repsByState(state).then(function(data) {
+        this.reps = data;
+      }.bind(this));
+    };
+
+    this.searchSensByName = function(name) {
+      reps.sensByName(name).then(function(data) {
+        this.reps = data;
+      }.bind(this));
+    };
+
+    this.searchSensByState = function(state) {
+      reps.sensByState(state).then(function(data) {
+        this.reps = data;
+      }.bind(this));
+    };
+  });
+
+angular.module('repsService', [])
+  .factory('reps', function($http){
+    var host = 'http://dgm-representatives.herokuapp.com';
+    return {
+      allByZip: function(zip) {
+        return $http
+          .get(host + '/all/by-zip/' + zip)
+          .then(function(res) {
+            return res.data;
+          });
+      },
+      repsByName: function(name) {
+        return $http
+          .get(host + '/reps/by-name/' + name)
+          .then(function(res){
+            return res.data;
+          });
+      },
+      repsByState: function(state) {
+        return $http
+          .get(host + '/reps/by-state/' + state)
+          .then(function(res) {
+            return res.data;
+          });
+      },
+      sensByName: function(name) {
+        return $http
+          .get(host + '/sens/by-name/' + name)
+          .then(function(res){
+            return res.data;
+          });
+      },
+      sensByState: function(state) {
+        return $http
+          .get(host + '/sens/by-state/' + state)
+          .then(function(res) {
+            return res.data;
+          });
+      }
     };
   });
