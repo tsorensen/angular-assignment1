1c652b6c3019f29f28ec1568646eca2c2f23acaf
Tyson Sorensen <tysonsorensen3@gmail.com>
2015-09-29 09:22:30 -0600

    added a few angular directives

--- client/js/app.js
+++ client/js/app.js
@@ -6,77 +6,67 @@ angular.module('RepsAppControllers', [
   'repsService'
 ])
   .controller('MainCtrl', function(reps) {
-    this.reps = [];
+    var self = this;
+    self.reps = [];
+    self.congressType = 'reps';
+    self.loading = false;
 
-    this.searchByZip = function(zip) {
-      reps.allByZip(zip).then(function(data) {
-        this.reps = data;
-      }.bind(this));
-    };
+    self.apis = [{
+      label: 'Zip',
+      method: function(zip) {
+        self.loading = true;
+        reps('all', 'zip', zip).then(function(data) {
+          self.loading = false;
+          self.reps = data;
+        });
+      }
+    },
+    {
+      label: 'Last Name',
+      method: function(name) {
+        self.loading = true;
+        reps(self.congressType, 'name', name).then(function(data) {
+          self.loading = false;
+          self.reps = data;
+        });
+      }
+    },
+    {
+      label: 'State',
+      method: function(state) {
+        self.loading = true;
+        reps(self.congressType, 'state', state).then(function(data) {
+          self.loading = false;
+          self.reps = data;
+        });
+      }
+    }];
+    self.criteria = self.apis[0];
 
-    this.searchRepsByName = function(name) {
-      reps.repsByName(name).then(function(data) {
-        this.reps = data;
-      }.bind(this));
-    };
-
-    this.searchRepsByState = function(state) {
-      reps.repsByState(state).then(function(data) {
-        this.reps = data;
-      }.bind(this));
-    };
-
-    this.searchSensByName = function(name) {
-      reps.sensByName(name).then(function(data) {
-        this.reps = data;
-      }.bind(this));
-    };
-
-    this.searchSensByState = function(state) {
-      reps.sensByState(state).then(function(data) {
-        this.reps = data;
-      }.bind(this));
-    };
   });
 
 angular.module('repsService', [])
   .factory('reps', function($http){
     var host = 'http://dgm-representatives.herokuapp.com';
-    return {
-      allByZip: function(zip) {
-        return $http
-          .get(host + '/all/by-zip/' + zip)
-          .then(function(res) {
-            return res.data;
-          });
-      },
-      repsByName: function(name) {
-        return $http
-          .get(host + '/reps/by-name/' + name)
-          .then(function(res){
-            return res.data;
-          });
-      },
-      repsByState: function(state) {
-        return $http
-          .get(host + '/reps/by-state/' + state)
-          .then(function(res) {
-            return res.data;
-          });
-      },
-      sensByName: function(name) {
-        return $http
-          .get(host + '/sens/by-name/' + name)
-          .then(function(res){
-            return res.data;
-          });
-      },
-      sensByState: function(state) {
-        return $http
-          .get(host + '/sens/by-state/' + state)
-          .then(function(res) {
-            return res.data;
-          });
-      }
-    };
+
+    /*
+     * @function search
+     * @param {String} type - can be "all", "reps", "sens"
+     * @param {String} query - can be any string
+     */
+    function search(type, criteria, query) {
+      return $http
+        .get(host + '/' + type + '/by-' + criteria + '/' + query)
+        .then(function(res){
+          return res.data;
+        });
+    }
+
+    search.allByZip = search.bind(null, 'all', 'zip');
+    search.repsByName = search.bind(null, 'reps', 'name');
+    search.repsByState = search.bind(null, 'reps', 'state');
+    search.sensByName = search.bind(null, 'sens', 'name');
+    search.sensByState = search.bind(null, 'sens', 'state');
+
+    return search;
   });
